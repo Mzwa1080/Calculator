cd446a9dc737a9dd18dccb2c5d650ecb01dfb6bf
Mzwa1080 <mshawnlouw@gmail.com>
2023-11-13 01:57:31 +0200

    made some few changes

--- script/index.js
+++ script/index.js
@@ -1,25 +1,28 @@
-let displayVal = document.getElementById('display')
+let displayVal = document.getElementById('output');
+let sum = '';
 
 
 function displayValue(val) {
     displayVal.value = displayVal.value + val;
  }
 
- function output() {
+// I must have the output function for the answer, displaying the values
+ function evaluateSum() {
     let num1 = displayVal.value;
     let num2 = eval(num1);   //I can also use the +num 
     displayVal.value = num2;
  }
 
+//  I must have a function that removes everything on the input(value)
  function erase() {
     console.log('Clear Reached!', displayVal.value)
-    displayVal.value = ' ';
+    displayVal.value = sum;
     // console.log('Clear not reached!');
  }
-
+// I must have a function to remove a single number
  function backSpace() {
     console.log('reached here')
-    display.value = display.value.slice(0, -1);
+    displayVal.value = displayVal.value.slice(0, -1);
  }
 
 
